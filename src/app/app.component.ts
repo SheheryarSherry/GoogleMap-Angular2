c71e85c695231afd0512adbea69a4456247be7b4
SheheryarSherry <sheheryarkhan1992@hotmail.com>
2017-01-22 02:58:03 +0500

    directives issues-- will be continued...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import {MapsAPILoader,NoOpMapsAPILoader,MouseEvent,ANGULAR2_GOOGLE_MAPS_PROVIDERS
+} from 'angular2-google-maps/core';
+interface marker{
+  lat:number;
+  lng:number;
+  lable?:string;
+  dragabble:boolean;
+  icon?:string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ANGULAR2_GOOGLE_MAPS_PROVIDERS]
 })
 export class AppComponent {
   title:string='Angular2 Google Map by Sheheryar';
