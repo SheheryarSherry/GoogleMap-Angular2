57f6abdc39ccd1e8c7bae75b0c0f03bcdc7c2a7e
SheheryarSherry <sheheryarkhan1992@hotmail.com>
2017-01-22 12:31:39 +0500

    installed bower

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import {MapsAPILoader,NoOpMapsAPILoader,MouseEvent,ANGULAR2_GOOGLE_MAPS_PROVIDERS
-} from 'angular2-google-maps/core';
-interface marker{
-  lat:number;
-  lng:number;
-  lable?:string;
-  dragabble:boolean;
-  icon?:string;
-}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[ANGULAR2_GOOGLE_MAPS_PROVIDERS]
 })
 export class AppComponent {
   title:string='Angular2 Google Map by Sheheryar';
@@ -24,18 +15,6 @@ export class AppComponent {
   clickedMarker(lable:string,index:number){
     console.log('click the marker: ${lable||index}')
   }
-  markers:marker[]=[
-    {
-      lat:-37.7863713,
-      lng:175.2796333,
-      dragabble:false
-    },
-    {
-      lat:-37.8253519,
-      lng:175.30493468,
-      dragabble:false
-    }
-
-  ];
+  
 }
 
